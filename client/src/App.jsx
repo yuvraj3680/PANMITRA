28f7c449fff10fea04e193f7904572d533aaf615
sai <yuvrajkhilari7625@gmail.com>
2024-07-05 15:55:25 +0530

    add cpanelfile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,25 @@
-import "./App.css";
 import React from 'react';
-import { BrowserRouter } from "react-router-dom"; 
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Homepage/Home';
-
+import Login from './components/User/Login';
 
 const App = () => {
+  // Simulate authentication check (replace with your actual logic)
+  const isAuthenticated = localStorage.getItem('token') !== null;
+
   return (
-   <> 
-      <BrowserRouter>
-        <Home/>
-      </BrowserRouter>
-     
-   </>
+    <Router>
+      <Routes>
+        {/* Route to login page */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Private route for home page */}
+        <Route
+          path="/*"
+          element={isAuthenticated ? <Home /> : <Navigate to="/login" replace />}
+        />
+      </Routes>
+    </Router>
   );
 };
 
